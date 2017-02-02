c4e1b5700d02d7d47d54c4b101a931f76e00e7b3
Filip Sufitchi <fsufitchi@gmail.com>
2017-02-01 19:51:52 -0500

    move new deps into vendor bundle

--- src/vendor.ts
+++ src/vendor.ts
@@ -2,15 +2,12 @@ import 'lodash';
 
 import '@angular/core';
 import '@angular/common';
-//import '@angular/http';
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
-//import '@ngrx/core';
-//import '@ngrx/store';
-//import '@ngrx/effects';
+import '@angular/router';
+import 'ngx-modal'
 
 import 'rxjs/Rx';
-//import 'protobufjs';
 import 'immutable';
 
 // import 'rxsocket';
